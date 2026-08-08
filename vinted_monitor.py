41822c9accb81fc8eafca291b1386466a8178116
lukas2104 <lukasrauch2104@gmail.com>
2026-08-08 19:52:04 +0200

    Update vinted_monitor.py

--- vinted_monitor.py
+++ vinted_monitor.py
@@ -51,9 +51,7 @@ def get_items():
 
         page.wait_for_timeout(5000)
 
-        items = page.locator('a[href*="/items/"]').evaluate_all("""
-print("GEFUNDENE VINTED-LINKS:", len(items))
-    links => links.map(link => ({
+        items = page.locator('a[href*="/items/"]').evaluate_all("""links => links.map(link => ({
         url: link.href,
         title: link.innerText.trim()
     }))
